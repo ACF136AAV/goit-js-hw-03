9181276a0b342c112cc8ec48a0c2c414a2ded01d
ACF136 <andrusacf@ukr.net>
2024-02-14 01:50:28 +0200

    complete task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,17 +1,25 @@
 `use strict`;
-function checkForSpam(message) {
-    const lowerCaseMessage = message.toLowerCase();
-    if (lowerCaseMessage.includes('spam') || lowerCaseMessage.includes('sale')) {
-        return true;
-    } else {
-        return false;
+function filterArray(numbers, value) {
+    const arr = [];
+    for (let i = 0; i <= numbers.length; i += 1) {
+        if (numbers[i] > value) {
+            arr.push(numbers[i]);
+        }
     }
+    return arr
 }
 
-console.log(checkForSpam("Latest technology news")); 
-console.log(checkForSpam("JavaScript weekly newsletter")); 
-console.log(checkForSpam("Get best sale offers now!")); 
-console.log(checkForSpam("Amazing SalE, only tonight!")); 
-console.log(checkForSpam("Trust me, this is not a spam message")); 
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!"));
-console.log(checkForSpam("[SPAM] How to earn fast money?")); 
+
+
+
+filterArray([1, 2, 3, 4, 5], 3)
+filterArray([1, 2, 3, 4, 5], 4)
+filterArray([1, 2, 3, 4, 5], 5)
+filterArray([12, 24, 8, 41, 76], 38)
+filterArray([12, 24, 8, 41, 76], 20)
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
